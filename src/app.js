10e96175a64a898117c10670a0d9e319000afbe4
portneon <shashwatsharmapalak@gamil.com>
2025-06-20 17:05:55 +0530

    download feature added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { jsPDF } from "jspdf";
+
 
 const App = () => {
   const [note, setNote] = useState("");
@@ -32,6 +34,22 @@ const App = () => {
     setSavedNote("");
     setWarning("");
   };
+  const Downloadnotes = (format) => {
+    const file = `note_${new Date().toISOString().slice(0, 19)}.${format}`;
+    if (format == "pdf") {
+      const doc = new jsPDF()
+      const lines = doc.splitTextToSize(savedNote, 180);
+      doc.text(lines, 10, 10)
+      doc.save(file)
+    } else {
+      const blob = new Blob([savedNote], { type: "text/plain" });
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob)
+      link.download = file
+      link.click()
+      
+    }
+  }
 
   return (
     <div style={styles.container}>
@@ -57,6 +75,9 @@ const App = () => {
         <div style={styles.saved}>
           <strong>🗒️ Saved Note:</strong>
           <p>{savedNote}</p>
+          <button onClick={() => Downloadnotes("txt")}>Download as .txt</button>
+          <button onClick={() => Downloadnotes("md")}>Download as .md</button>
+          <button onClick={() => Downloadnotes("pdf")}>Download as .pdf</button>
         </div>
       )}
     </div>
